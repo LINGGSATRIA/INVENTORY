0a8b992e402125a99880706b13bc91be3c8d4a3c
wildanu <wildansbh@gmail.com>
2024-12-29 03:17:07 +0700

    Add Dashboard

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,8 @@ class Home extends BaseController
     {
         return view('login');
     }
+    public function Dash(): string
+    {
+        return view('Dash');
+    }
 }
---
+++ app/Views/Component/C_Template.php
@@ -0,0 +1,5 @@
+<?= $this->include('Component/C_Header') ?>
+<?= $this->include('Component/C_Side') ?>
+<?= $this->include('Component/C_Top') ?>
+<?= $this->renderSection('konten') ?>
+<?= $this->include('Component/C_Footer') ?>
