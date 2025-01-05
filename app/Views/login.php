7b35dd0430e6759bc1aefbb17b04984762098a92
lingga <linggasatria099@gmail.com>
2025-01-05 18:11:21 +0700

    update name

--- app/Views/login.php
+++ app/Views/login.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>KAVIANCE</title>
+    <title>SILEO</title>
 
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -34,7 +34,7 @@
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-2">KAVIANCE</h1>
+                                        <h1 class="h4 text-gray-900 mb-2">SI LEO</h1>
                                     </div>
                                     <?php if ($errors = session()->getFlashdata('error')): ?>
                                         <div class="alert alert-danger">
